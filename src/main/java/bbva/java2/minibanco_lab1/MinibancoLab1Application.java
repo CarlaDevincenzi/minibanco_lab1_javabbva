20f88f7dd18d2d2966cd55212c58fa2f2defd0ee
CarlaDevincenzi <anabellacarla2301@gmail.com>
2023-05-16 16:01:34 -0300

    first commit

---
+++ src/main/java/bbva/java2/minibanco_lab1/MinibancoLab1Application.java
@@ -0,0 +1,13 @@
+package bbva.java2.minibanco_lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinibancoLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinibancoLab1Application.class, args);
+	}
+
+}
---
+++ src/test/java/bbva/java2/minibanco_lab1/MinibancoLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package bbva.java2.minibanco_lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinibancoLab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
